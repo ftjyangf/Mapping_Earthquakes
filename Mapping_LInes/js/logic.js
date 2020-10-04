379f8430a99266673437cda4136aac7a6d3af0cf
unknown <ftjyangf@gmail.com>
2020-10-03 22:19:06 -0700

    draw a line on map

--- Mapping_LInes/js/logic.js
+++ Mapping_LInes/js/logic.js
@@ -1,36 +1,24 @@
 console.log('yes')
 
 
-let mymap = L.map('mapid').setView([34.0522, -118.2437], 14);
+let mymap = L.map('mapid').setView([39.9416, -100.4085],5);
 
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/outdoors-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
-
-
-
-// L.circleMarker([34.0522, -118.2437],
-//     {
-//         radius:100,
-//         color:'black',
-//         fillColor:'#99a3b2'
-//     }
-//     ).addTo(mymap);
-
-
 streets.addTo(mymap);
-var circle = L.circle([34.0522, -118.2437], {
+var circle = L.circle([51.508, -0.11], {
     color: 'blue',
     fillColor: '#f03',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 700
 }).addTo(mymap);
 
 var polygon = L.polygon([
@@ -42,7 +30,40 @@ var polygon = L.polygon([
 
 
 
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790]
+  ]
+
+L.polyline(line,{
+    color: "red",
+}).addTo(mymap)
 
 
+let line1 = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [47.4502, -122.3088],
+    [40.7899, -111.9791]
+    
+  ];
 
 
+  L.polyline(line1,{
+      color:'yellow',
+  }).addTo(mymap)
+
+  let line2 = [
+  [33.9416, -118.4085],
+  [30.1975, -97.6664],
+  [43.6777,-79.6248],
+  [40.6413,-73.7781],
+  
+  ]
+
+  L.polyline(line2,{
+      color:'blue',
+      opacity:0.8,
+      dashArray: "5",
+      weight:4
+      }).addTo(mymap)
