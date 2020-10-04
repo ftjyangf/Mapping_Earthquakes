0faaea2fa65655f04eb87ad9180e5d628f48426a
unknown <ftjyangf@gmail.com>
2020-10-03 17:25:18 -0700

    single point map with circle adjustment

--- js/logic.js
+++ js/logic.js
@@ -7,12 +7,24 @@ let mymap = L.map('mapid').setView([34.0522, -118.2437], 14);
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/satellite-v9',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
+
+
+
+L.circleMarker([34.0522, -118.2437],
+    {
+        radius:300,
+        color:'black',
+        fillColor:'#99a3b2'
+    }
+    ).addTo(mymap);
+
+
 streets.addTo(mymap);
 var circle = L.circle([34.0522, -118.2437], {
     color: 'blue',
@@ -27,3 +39,4 @@ var polygon = L.polygon([
     [51.51, -0.047],
     [51.8, -0.03]
 ]).addTo(mymap);
+
